46d7e736a43eb1acf58e32abf220ef3ed08817ae
Usama Arshad <usama.lhr@gmail.com>
2018-02-01 02:36:37 +0500

    renamed variable

--- flatten_hash.rb
+++ flatten_hash.rb
@@ -1,13 +1,13 @@
-@a = {}
+@flat_hash = {}
 def flatten_hash(hash)
   return unless hash.is_a? Hash
   hash.each_pair do |key, value|
     if value.is_a? Hash
-      @a.merge!({"#{key}".to_sym => "#{value.keys.first}"})
+      @flat_hash.merge!({"#{key}".to_sym => "#{value.keys.first}"})
     else
-      @a.merge!("#{key}".to_sym => value)
+      @flat_hash.merge!("#{key}".to_sym => value)
     end
     flatten_hash(value)
   end
-  @a
+  @flat_hash
 end
